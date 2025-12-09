37dc19505fea8472904bbe9da49b463a8837d031
Robin Roelandt <Robin.Roelandt@LAP-5CG05109HV.vivesnet.be>
2025-12-09 09:52:52 +0100

    first commit

---
+++ Galleries/Galleries/GalleriesApp.swift
@@ -0,0 +1,17 @@
+//
+//  GalleriesApp.swift
+//  Galleries
+//
+//  Created by Robin Roelandt on 09/12/2025.
+//
+
+import SwiftUI
+
+@main
+struct GalleriesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
